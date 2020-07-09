27c9783d8ab300e6386002f19e7a4e6286b664c8
jonathangjertsen <jonath.re@gmail.com>
2020-07-09 15:57:30 +0200

    Add missing file to Arduino sketch folder

--- build_script.sh
+++ build_script.sh
@@ -8,7 +8,7 @@ cmake .. -G Ninja
 ninja ctest
 
 # Copy lib onto sketch
-cp ../lib/inc/dht11lib.h ../lib/src/dht11lib.c ../arduino/dht11
+cp ../lib/inc/dht11lib.h ../lib/src/dht11lib.c ../lib/src/dht11_arduino_constants.c ../arduino/dht11
 
 # Compile sketch
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
